07f47d3d8d2ee5fe48397f273e3a0e7321f8ceef
Ahmed <ahmedelsharawy117@gmail.com>
2026-05-10 15:54:37 +0300

    feat: generate assembly code for binary expressions

--- src/codegen.c
+++ src/codegen.c
@@ -8,6 +8,7 @@ static void gen_expr(ASTNode *node, FILE *out);
 static void gen_stmt(ASTNode *node, FILE *out);
 
 static void gen_function(ASTNode *node, FILE *out) {
+  fprintf(out, ".global %s\n\n", node->function_decl.name);
   fprintf(out, "%s:\n", node->function_decl.name);
   gen_stmt(node->function_decl.body, out);
 }
@@ -32,6 +33,30 @@ static void gen_expr(ASTNode *node, FILE *out) {
   switch (node->type) {
   case AST_NUMBER:
     fprintf(out, "    mov rax, %d\n", node->number.value);
+    break;
+  case AST_BINARY_OP:
+    gen_expr(node->binary_op.left, out);
+    fprintf(out, "    push rax\n");
+    gen_expr(node->binary_op.right, out);
+    fprintf(out, "    mov rbx, rax\n");
+    fprintf(out, "    pop rax\n");
+    switch (*node->binary_op.op.value) {
+    case '+':
+      fprintf(out, "    add rax, rbx\n");
+      break;
+    case '-':
+      fprintf(out, "    sub rax, rbx\n");
+      break;
+    case '*':
+      fprintf(out, "    imul rax, rbx\n");
+      break;
+    case '/':
+      fprintf(out, "    cqo\n");
+      fprintf(out, "    idiv rbx\n");
+      break;
+    default:
+      break;
+    }
     break;
   default:
     break;
