b0bd9c412c18df464420bcef1be0810ec61131c8
Ahmed <ahmedelsharawy117@gmail.com>
2026-05-11 11:41:49 +0300

    feat(codegen): add helper function to find local variables and handle writing variables declaration and variable assignment

--- src/codegen.c
+++ src/codegen.c
@@ -1,29 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "../include/codegen.h"
+
+LocalVar locals[256];
+int local_count;
+int local_offset;
+
+static int find_local(const char *name) {
+  for (int i = 0; i < local_count; i++) {
+    if (strcmp(locals[i].name, name) == 0) {
+      return locals[i].offset;
+    }
+  }
+  fprintf(stderr, "Undefined variable: %s\n", name);
+  exit(1);
+}
 
 static void gen_function(ASTNode *node, FILE *out);
 static void gen_expr(ASTNode *node, FILE *out);
 static void gen_stmt(ASTNode *node, FILE *out);
 
 static void gen_function(ASTNode *node, FILE *out) {
+  local_count = 0;
+  local_offset = 0;
   fprintf(out, ".global %s\n\n", node->function_decl.name);
   fprintf(out, "%s:\n", node->function_decl.name);
+  fprintf(out, "    push rbp\n");
+  fprintf(out, "    mov rbp, rsp\n");
+  fprintf(out, "    sub rsp, 256\n");
+
   gen_stmt(node->function_decl.body, out);
 }
 
 static void gen_stmt(ASTNode *node, FILE *out) {
   switch (node->type) {
-  case AST_RETURN:
+  case AST_VARIABLE_DECL: {
+    gen_expr(node->variable_decl.expr, out);
+    local_offset += 8;
+    locals[local_count].name = node->variable_decl.name;
+    locals[local_count].offset = local_offset;
+    local_count++;
+    fprintf(out, "    mov [rbp-%d], rax\n", local_offset);
+    break;
+  }
+
+  case AST_ASSIGN: {
+    gen_expr(node->assign.expr, out);
+    int offset = find_local(node->assign.name);
+    fprintf(out, "    mov [rbp-%d], rax\n", offset);
+    break;
+  }
+  case AST_RETURN: {
     gen_expr(node->return_stmt.expr, out);
+    fprintf(out, "    mov rsp, rbp\n");
+    fprintf(out, "    pop rbp\n");
     fprintf(out, "    ret\n");
     break;
-  case AST_BLOCK:
+  }
+  case AST_BLOCK: {
     for (size_t i = 0; i < node->block.statement_count; i++) {
       gen_stmt(node->block.statements[i], out);
     }
     break;
+  }
   default:
     break;
   }
@@ -31,27 +72,37 @@ static void gen_stmt(ASTNode *node, FILE *out) {
 
 static void gen_expr(ASTNode *node, FILE *out) {
   switch (node->type) {
-  case AST_NUMBER:
+  case AST_NUMBER: {
     fprintf(out, "    mov rax, %d\n", node->number.value);
     break;
-  case AST_UNARY_OP:
+  }
+  case AST_VARIABLE: {
+    int offset = find_local(node->variable.name);
+    fprintf(out, "    mov rax, [rbp-%d]\n", offset);
+    break;
+  }
+  case AST_UNARY_OP: {
     gen_expr(node->unary_op.operand, out);
     switch (*node->unary_op.op.value) {
-    case '-':
+    case '-': {
       fprintf(out, "    neg rax\n");
       break;
-    case '!':
+    }
+    case '!': {
       fprintf(out, "    cmp rax, 0\n");
       fprintf(out, "    sete al\n");
       fprintf(out, "    movzb rax, al\n");
       break;
-    case '~':
+    }
+    case '~': {
       fprintf(out, "    not rax\n");
       break;
+    }
     default:
       break;
     }
     break;
+  }
   case AST_BINARY_OP:
     gen_expr(node->binary_op.left, out);
     fprintf(out, "    push rax\n");
