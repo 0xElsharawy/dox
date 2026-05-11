1f887d329f87ab48b7c46cc478432c6db7b25b4b
Ahmed <ahmedelsharawy117@gmail.com>
2026-05-11 11:35:55 +0300

    feat: add nodes for variable declatation and variable and assign and structures for them

--- include/ast.h
+++ include/ast.h
@@ -7,13 +7,15 @@
 
 typedef enum {
   AST_NUMBER,
-  AST_IDENTIFIER,
   AST_RETURN,
   AST_BINARY_OP,
   AST_UNARY_OP,
   AST_FUNCTION_CALL,
   AST_FUNCTION_DECL,
   AST_BLOCK,
+  AST_VARIABLE_DECL,
+  AST_VARIABLE,
+  AST_ASSIGN,
 } ASTNodeType;
 
 typedef struct ASTNode {
@@ -23,10 +25,6 @@ typedef struct ASTNode {
     struct {
       int value;
     } number;
-
-    struct {
-      char *name;
-    } identifier;
 
     struct {
       struct ASTNode *expr;
@@ -62,17 +60,34 @@ typedef struct ASTNode {
       Token op;
       struct ASTNode *operand;
     } unary_op;
+
+    struct {
+      char *name;
+      char *type;
+      struct ASTNode *expr;
+    } variable_decl;
+
+    struct {
+      char *name;
+    } variable;
+
+    struct {
+      char *name;
+      struct ASTNode *expr;
+    } assign;
   };
 } ASTNode;
 
 ASTNode *ast_number(int value);
-ASTNode *ast_identifier(const char *name);
 ASTNode *ast_function_decl(const char *name, const char *return_type,
                            ASTNode *body);
 ASTNode *ast_block(ASTNode **statements, size_t count);
 ASTNode *ast_return(ASTNode *expr);
 ASTNode *ast_binary_op(Token op, ASTNode *left, ASTNode *right);
 ASTNode *ast_unary_op(Token op, ASTNode *operand);
+ASTNode *ast_variable_decl(const char *name, const char *type, ASTNode *expr);
+ASTNode *ast_variable(const char *name);
+ASTNode *ast_assign(const char *name, ASTNode *expr);
 
 void print_ast(const ASTNode *node, int depth);
 
